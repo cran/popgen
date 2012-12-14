c8c6917ec9500070469a2d26b845be7fbcd6abbe
J L Marchini <marchini@stats.ox.ac.uk>
2012-12-14 00:00:00 +0000

    version 1.0-2

--- R/zzz.r
+++ R/zzz.r
@@ -1,4 +1,5 @@
 
-.First.lib <- function(lib, pkg){
-    library.dynam("popgen", pkg, lib)
+.onLoad <- function(lib, pkg){
+     # do whatever needs to be done when the package is loaded
+      library.dynam("popgen", pkg, lib)
 }
